b3edeb78480ddf742535904183d08b72bd009a5e
Svetoslaus <105913393+Svetoslaus@users.noreply.github.com>
2023-03-07 23:25:32 +0100

    Create carly-s_clippers.py

---
+++ carly-s_clippers.py
@@ -0,0 +1,26 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+  total_price = total_price + price
+
+#average price
+average_price = total_price / len(prices)
+
+#new prices
+new_prices = [price - 5 for price in prices ]
+
+total_revenue = []
+for i in range(len(hairstyles)):
+   total_revenue = prices[i] +     last_week[i]
+
+average_daily_revenue = total_revenue / 7   
+
+cuts_under_30 = [new_prices[i] for i in range(len(new_prices)-1) if new_prices[i] < 30]
+
+print("Average Haircut Price: " + str(cuts_under_30 ))
